28e89cd6e07d5a20d4fbd196141257e84f593100
Emma Paris <davidadediji@gmail.com>
2022-05-16 16:56:10 +0100

    add card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Card from './components/Card';
+import CardData from './data';
 import './style.css';
-
+const CardElements = CardData.map((ele)=>{
+  return <Card
+    image = {ele.coverImg}
+    country = {ele.location}
+    rating = {ele.stats.rating}
+    review = {ele.stats.reviewCount}
+    title = {ele.title}
+    price = {ele.price}
+  />
+})
 function App() {
+
   return (
     <div className="container">
       <Navbar />
       <Hero />
+      {CardElements}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,19 @@
+import Star from "../img/Star1.png"
 
-function Card(){
+function Card(props){
     return(
-        <div>
-            
+        <div className="card">                                                 
+            <img className="card-img" src={props.image} alt="first-card"/>
+            <div className="card-stat">
+                <img className="card-star" src={Star} alt=""/>
+                <span>{props.rating}</span>
+                <span className="gray">({props.review}) . {props.country}</span>
+            </div>
+            <p>{props.title}</p>
+            <p><span class="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
+
+
+export default Card;
